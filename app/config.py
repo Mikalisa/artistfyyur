d6dde6af4cac7b859ff53a75d93c9f193671001d
Michel <mekalissa0@gmail.com>
2021-07-16 08:22:47 -0400

    add environment variable

--- app/config.py
+++ app/config.py
@@ -1,9 +1,23 @@
 import os
 
-
-SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
+
+# Database Config
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+# Session Config
+
+SESSION_TYPE = "filesystem"
+
+
+# Whoosh Config
+WHOOSH_BASE = 'whoosh'
+
