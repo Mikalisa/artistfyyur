9315969ba3caedb9c50fe8fd8624ac320b2f54dd
Michel <mekalissa0@gmail.com>
2021-07-16 07:57:43 -0400

    first commit

---
+++ app/settings.py
@@ -0,0 +1,18 @@
+import os
+from tempfile import mkdtemp
+
+# Database Config
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+# Session Config
+
+SESSION_TYPE = "filesystem"
+
+
+# Whoosh Config
+WHOOSH_BASE = 'whoosh'
+
